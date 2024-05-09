b5a1dbec49479564285ba7e15690fd63da05bafc
Jonathan Wills <jwills@bungalow12.com>
2024-05-09 16:27:26 -0700

    Update scripts and workflows (#2)
    * Update scripts and workflows
    
    * Fix minimal test placeholder

--- packages/core/src/plugin/Input.spec.ts
+++ packages/core/src/plugin/Input.spec.ts
@@ -1,5 +1,13 @@
+import { JSDOM } from 'jsdom';
+import Input from './Input';
+
+jest.mock('three');
+
 describe('Input', () => {
   test('Placeholder', () => {
     // TODO: Add the tests for Input asap
+    const dom = new JSDOM();
+    const domElement = dom.window.document.createElement('canvas');
+    const input = new Input(domElement);
   });
 });
