a81f177c37988b362f8b7cdf43406a8b47da04f8
Jonathan Wills <jwills@bungalow12.com>
2024-05-09 16:03:01 -0700

    Fix minimal test placeholder

--- packages/core/src/plugin/Input.spec.ts
+++ packages/core/src/plugin/Input.spec.ts
@@ -1,5 +1,10 @@
+import Unithree from '../';
+import Input from './Input';
+
 describe('Input', () => {
   test('Placeholder', () => {
     // TODO: Add the tests for Input asap
+    Unithree.initialize();
+    const input = new Input(Unithree.getRenderer().domElement);
   });
 });
